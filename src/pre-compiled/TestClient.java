7569d9a925f425a7229693300eabc4b8f9ee97fa
Armlete <72060798+Armlete@users.noreply.github.com>
2023-05-29 17:41:40 +1000

    Final

--- src/pre-compiled/TestClient.java
+++ src/pre-compiled/TestClient.java
@@ -2,103 +2,91 @@ import java.io.*;
 import java.net.*;
 
 public class TestClient {
+	public static void write(DataOutputStream dout, String message) {
+		try {
+			dout.write((message + "\n").getBytes());
+			dout.flush();
+		} catch (
+
+		Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
 	public static void main(String[] args) {
 		try {
 			Socket s = new Socket("localhost", 50000);
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 			BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			dout.write(("HELO\n").getBytes());
-			dout.flush();
+			write(dout,"HELO");
 			din.readLine(); // should be OK
-			dout.write(("AUTH me\n").getBytes());
-			dout.flush();
+			write(dout,"AUTH me");
 			din.readLine(); // should be ok
 			// Start
-			dout.write(("REDY\n").getBytes());
-			dout.flush();
+			write(dout,"REDY");
 			String temp = din.readLine(); // reads JOBN and ignores it //can optimise maybe
-			dout.write(("GETS All\n").getBytes());
-			dout.flush();
+			write(dout,"GETS All");
 			Integer sData = Integer.parseInt(din.readLine().split(" ")[1]);// reads DATA
 			String[][] servers = new String[sData][9];
-			dout.write(("OK\n").getBytes());
-			dout.flush();
+			write(dout,"OK");
 			for (int i = 0; i < servers.length; i++) {
 				servers[i] = din.readLine().split(" "); // reads Server Info
 			}
-			dout.write(("OK\n").getBytes());
-			dout.flush();
-			din.readLine(); // reads*/
-			String[] job = new String[7]; // can optimise
-			Integer bTime;
-			Integer sTime;
-			Integer sCore;
-			Integer sMem;
-			Integer sDisk;
-			Integer jID;
-			Integer jTime;
-			Integer jCore;
-			Integer jMem;
-			Integer jDisk;
-			Integer bNum;
-			Integer sRun;
-			String sID = "";
-			String sType = "";
+			write(dout,"OK");
+			din.readLine(); // reads . 
+			String[] job = new String[7];
+			Integer bTime;//Best time
+			Integer bNum;//Best server number
+			String sID = "";//ServerID
+			String sType = "";//ServerType
+			Integer sTime;//Server Time
+			Integer sCore;//Server Core
+			Integer sMem;//Server Memory
+			Integer sDisk;//Server Disk
+			Integer jID;//Job ID
+			Integer jTime;//Job Time
+			Integer jCore;//Job Core
+			Integer jMem;//Job Memory
+			Integer jDisk;//Job Disk
 			while (true) {
-				if (temp.contains("NONE")) {
+				if (temp.contains("NONE")) {//End program
 					System.out.println("NONE");
 					break;
-				}
-				else if (temp.contains("JCPL")) {
-				} 
-				else if (temp.contains("JOBN")) {
-					dout.write(("GETS All\n").getBytes());
-					dout.flush();
-					String test = din.readLine();
-					System.out.println(test + "Not Data??");
-					sData = Integer.parseInt(test.split(" ")[1]);// reads DATA
-					dout.write(("OK\n").getBytes());
-					dout.flush();
-					String timeTemp;
-					String ok;
-					for (int i = 0; i < servers.length; i++) {
-						//System.out.println("Before");
-						timeTemp = servers[i][3];
-						ok = din.readLine();
-						System.out.println(ok + " This should be a server");
-						servers[i] = ok.split(" "); // reads Server Info
-						servers[i][3] = timeTemp;
-						//System.out.println("After");
-					}
-					dout.write(("OK\n").getBytes());
-					dout.flush();
-					din.readLine();
-					//System.out.println(din.readLine() + " = .");	
-
+				} else if (temp.contains("JCPL")) {//skip JOBN and restart the loop
+				} else if (temp.contains("JOBN")) {
 					job = temp.split(" ");
-					//System.out.println(temp + " This is Temp");
 					jID = Integer.parseInt(job[2]);
 					jTime = Integer.parseInt(job[3]);
 					jCore = Integer.parseInt(job[4]);
 					jMem = Integer.parseInt(job[5]);
 					jDisk = Integer.parseInt(job[6]);
-					bTime = 0; // holds shortest wait time
-					bNum = -1; // holds chosen server
+					write(dout,"GETS All");
+					sData = Integer.parseInt(din.readLine().split(" ")[1]);// reads DATA
+					write(dout,"OK");
+					String timeTemp;
+					String serverTemp;
+					for (int i = 0; i < servers.length; i++) {
+						timeTemp = servers[i][3];//hold current estimated job time 
+						serverTemp = din.readLine();
+						servers[i] = serverTemp.split(" "); // reads Server Info
+						servers[i][3] = timeTemp;//replaces current estimated job time
+					}
+					write(dout,"OK");
+					din.readLine();//reads . 
+
+					bTime = 0; // holds best/shortest wait time
+					bNum = -1; // holds best/chosen server
 					for (int i = 0; i < servers.length; i++) {
 						sCore = Integer.parseInt(servers[i][4]);
 						sMem = Integer.parseInt(servers[i][5]);
 						sDisk = Integer.parseInt(servers[i][6]);
-						sRun = Integer.parseInt(servers[i][7]);
 						sTime = Integer.parseInt(servers[i][3]);
 						if (jCore <= sCore && jMem <= sMem && jDisk <= sDisk) {
 							if (sTime == 0) {
 								bNum = i;
 								break;
 							}
-							/*if (sRun>0){
-								bNum = i;
-								break;
-							}*/
 							if (bTime == 0) {
 								bTime = sTime;
 								bNum = i;
@@ -109,16 +97,13 @@ public class TestClient {
 						}
 					}
 					if (bNum == -1) {
-						System.out.println("bNum is -1");
-						dout.write(("GETS Capable " + Integer.toString(jCore) + " " + Integer.toString(jMem) + " "
-								+ Integer.toString(jDisk) + "\n").getBytes());
-						dout.flush();
-						sData = Integer.parseInt(din.readLine().split(" ")[1]);
-						dout.write(("OK\n").getBytes());
-						dout.flush();
+						write(dout,"GETS Capable " + Integer.toString(jCore) + " " + Integer.toString(jMem) + " "
+						+ Integer.toString(jDisk));
+						sData = Integer.parseInt(din.readLine().split(" ")[1]);//Reads number of servers about to be sent
+						write(dout,"OK");
 						String[] serverTemp2;
 						Integer lowJobs = 999;
-						for (int i = 0; i < sData; i++) {
+						for (int i = 0; i < sData; i++) {//reads server info
 							serverTemp2 = din.readLine().split(" ");
 							if (Integer.parseInt(serverTemp2[7]) < lowJobs) {
 								lowJobs = Integer.parseInt(serverTemp2[7]);
@@ -126,34 +111,29 @@ public class TestClient {
 								sID = serverTemp2[1];
 							}
 						}
-						System.out.println("Hello");
-						dout.write(("OK\n").getBytes());
-						dout.flush();
-						System.out.println(din.readLine()+" ????");
-						dout.write(("SCHD " + jID + " " + sType + " " + sID + "\n").getBytes());
-						dout.flush();
-						for(int i = 0;i<servers.length;i++){
-							if(servers[i][0]==sType&&servers[i][1]==sID){
+						// System.out.println("Hello");
+						write(dout,"OK");
+						din.readLine();//Reads .
+						write(dout,"SCHD " + jID + " " + sType + " " + sID);
+						for (int i = 0; i < servers.length; i++) {//Add time to server list 
+							if (servers[i][0] == sType && servers[i][1] == sID) {
 								servers[i][3] = Integer.toString(Integer.parseInt(servers[i][3]) + jTime);
 							}
 						}
-						System.out.println(din.readLine() + " = OK"); // reads OK
+						din.readLine();//Reads OK
 					} else {
-						dout.write(("SCHD " + jID + " " + sType + " " + sID + "\n").getBytes());
-						dout.flush();
-						servers[bNum][3] = Integer.toString(Integer.parseInt(servers[bNum][3]) + jTime);
-						System.out.println(din.readLine()); // reads OK
+						write(dout,"SCHD " + jID + " " + sType + " " + sID);
+						din.readLine();//Reads OK
+						servers[bNum][3] = Integer.toString(Integer.parseInt(servers[bNum][3]) + jTime);//Changing the job timings
 						sID = servers[bNum][1];
 						sType = servers[bNum][0];
 					}
 
 				}
-				dout.write(("REDY\n").getBytes());
-				dout.flush();
-				temp = din.readLine();
-				System.out.println("Temp at the end of the loop " +temp);
+				write(dout,"REDY");
+				temp = din.readLine();//Restarts the loop with temp being the holder
 			}
-			dout.write(("QUIT\n").getBytes());
+			write(dout,"QUIT");//Quitting
 			din.readLine();
 			din.close();
 			dout.close();
